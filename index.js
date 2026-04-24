91e2231c2522bf9b9c83117440b8980853b360f2
ravina-technobrave <ravina@technobrave.com>
2026-04-24 11:13:44 +0530

    initial commit with product fetch api sample

---
+++ index.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+require("dotenv").config();
+const SHOP = process.env.SHOPIFY_STORE;
+const TOKEN = process.env.SHOPIFY_ACCESS_TOKEN;
+
+
+async function fetchProducts() {
+  try {
+    const response = await axios.get(
+      `https://${SHOP}/admin/api/2025-01/products.json`,
+      {
+        headers: {
+          "X-Shopify-Access-Token": TOKEN,
+          "Content-Type": "application/json"
+        },
+      }
+    );
+
+    console.log(response.data.products);
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+  }
+}
+
+fetchProducts();
